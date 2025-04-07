bbc7f43c22d6305128fae96d55d771e6af4f4c9e
Jumballaya <nchupacabra@gmail.com>
2025-04-07 11:01:33 -0500

    Add load/get methods to the resource manager

--- src/week01_resource_manager/ResourceManager.hpp
+++ src/week01_resource_manager/ResourceManager.hpp
@@ -10,10 +10,32 @@ class ResourceManager
 {
 public:
   ResourceManager() = default;
-  std::shared_ptr<T> load(const std::string &name, std::function<std::shared_ptr<T>()> loader);
-  std::shared_ptr<T> get(const std::string &name) const;
-  void unload(const std::string &name);
-  void clear();
+
+  std::shared_ptr<T> load(const std::string &name, std::function<std::shared_ptr<T>()> loader)
+  {
+    auto found = resources_.find(name);
+    if (found != resources_.end())
+    {
+      return found->second;
+    }
+    auto loaded = loader();
+    resources_[name] = loaded;
+    return loaded;
+  }
+
+  std::shared_ptr<T> get(const std::string &name) const
+  {
+    auto found = resources_.find(name);
+    if (found == resources_.end())
+    {
+      return std::shared_ptr<T>{};
+    }
+    return found->second;
+  };
+
+  void unload(const std::string &name) {};
+
+  void clear() {};
 
 private:
   std::unordered_map<std::string, std::shared_ptr<T>> resources_;
--- src/week01_resource_manager/main.cpp
+++ src/week01_resource_manager/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <string>
+#include <memory>
+#include "ResourceManager.hpp"
 
 int main()
 {
-  std::cout << "Hello World" << std::endl;
+  ResourceManager<std::string> manager;
+
+  manager.load("greeting", []()
+               { return std::make_shared<std::string>("Hello World\n"); });
+
+  auto val = manager.get("greeting");
+  if (!val)
+  {
+    std::cout << "Greeting not found" << std::endl;
+    return 0;
+  }
+
+  std::cout << *manager.get("greeting");
 }
