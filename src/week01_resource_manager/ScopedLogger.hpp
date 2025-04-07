2663b6e7bd3d4e736d8c0981deeab3a90563d5c6
Jumballaya <nchupacabra@gmail.com>
2025-04-07 15:25:39 -0500

    WEEK_01: Add ScopeLogger assignment

---
+++ src/week01_resource_manager/ScopedLogger.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <string_view>
+
+class ScopedLogger {
+ public:
+  ScopedLogger(std::string_view label, bool enabled = true) : _label(label), _enabled(enabled) {
+    if (_enabled) {
+      std::cout << "⏱️ Entering: " << _label << std::endl;
+    }
+  }
+
+  // Make sure the object can't be copied or assigned!
+  ScopedLogger(const ScopedLogger&) = delete;
+  ScopedLogger& operator=(const ScopedLogger&) = delete;
+
+  ~ScopedLogger() {
+    if (logging) {
+      end();
+    }
+  }
+
+  void end() {
+    if (_enabled) {
+      std::cout << "✅ Exiting: " << _label << std::endl;
+    }
+    logging = false;
+  };
+
+ private:
+  const std::string _label;
+  bool logging = true;
+  bool _enabled = true;
+};
--- src/week01_resource_manager/main.cpp
+++ src/week01_resource_manager/main.cpp
@@ -3,15 +3,23 @@
 #include <string>
 
 #include "ResourceManager.hpp"
+#include "ScopedLogger.hpp"
+
+#define LOGGING_ENABLED false
 
 int main() {
+  ScopedLogger log_main("Main Block", LOGGING_ENABLED);
+
+  ScopedLogger log_resource_building("Resource Creation", LOGGING_ENABLED);
   ResourceManager<std::string> manager;
   manager.load("greeting", []() { return std::make_shared<std::string>("Hello World\n"); });
   manager.load("new-greeting", []() { return std::make_shared<std::string>("Hello Galaxy\n"); });
   manager.unload("greeting");
+  log_resource_building.end();
 
   manager.clear();
 
+  ScopedLogger log_resource_display("Resource Display", LOGGING_ENABLED);
   auto val = manager.get("greeting");
   if (!val) {
     val = manager.get("new-greeting");
@@ -20,6 +28,7 @@ int main() {
       return 0;
     }
   }
+  log_resource_display.end();
 
   std::cout << *val;
 }
