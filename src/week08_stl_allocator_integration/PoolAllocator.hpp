6c333c0227c9b29d129ac560edbc754afcac9b6b
Patrick Burris <nchupacabra@gmail.com>
2025-05-21 13:01:10 -0500

    WEEK 08: Add the basic PoolAllocator and PoolResource classes

--- src/week08_stl_allocator_integration/PoolAllocator.hpp
+++ src/week08_stl_allocator_integration/PoolAllocator.hpp
@@ -1,27 +1,98 @@
 #pragma once
 
+#include <cassert>
 #include <cstddef>
+#include <cstdint>
 #include <cstdlib>
+#include <memory>
 
+// PoolAllocator is non-owning. It is safe to move, but the caller must ensure
+// the backing buffer remains valid for the lifetime of all moved-from and
+// moved-to instances.
 class PoolAllocator {
  public:
   PoolAllocator(std::byte* buffer, size_t slotSize, size_t capacity)
       : _buffer(buffer), _slotSize(slotSize), _capacity(capacity), _usedCount(0) {
-    auto tail = _freeListHead;
-    for (size_t i = 0; i < capacity; ++i) {
+    // This is all to make sure that the buffer and the slotSize are properly
+    // aligned
+    assert(buffer != nullptr);
+    assert(reinterpret_cast<uintptr_t>(_buffer) % alignof(std::max_align_t) == 0);
+    assert(_slotSize >= sizeof(void*));
+    assert(_slotSize % alignof(std::max_align_t) == 0);
+
+    for (size_t i = 0; i < capacity - 1; ++i) {
+      std::byte* cur = buffer + i * slotSize;
+      std::byte* next = buffer + (i + 1) * slotSize;
+      *reinterpret_cast<void**>(cur) = next;
     }
+    void* last = _buffer + (_capacity - 1) * _slotSize;
+    *reinterpret_cast<void**>(last) = nullptr;
+    _freeListHead = buffer;
   }
 
   ~PoolAllocator() = default;
   PoolAllocator(const PoolAllocator&) = delete;
   PoolAllocator& operator=(const PoolAllocator&) = delete;
 
-  PoolAllocator(PoolAllocator&& other);
-  PoolAllocator& operator=(PoolAllocator&& other);
+  PoolAllocator(PoolAllocator&& other)
+      : _buffer(other._buffer),
+        _slotSize(other._slotSize),
+        _capacity(other._capacity),
+        _usedCount(other._usedCount),
+        _freeListHead(other._freeListHead) {
+    other._slotSize = 0;
+    other._capacity = 0;
+    other._usedCount = 0;
+    other._freeListHead = nullptr;
+  }
+  PoolAllocator& operator=(PoolAllocator&& other) {
+    if (this == &other) return *this;
 
-  void* allocate();
-  void deallocate(void* ptr);
-  bool owns(void* ptr) const;
+    _buffer = other._buffer;
+    _slotSize = other._slotSize;
+    _capacity = other._capacity;
+    _usedCount = other._usedCount;
+    _freeListHead = other._freeListHead;
+
+    other._buffer = nullptr;
+    other._slotSize = 0;
+    other._capacity = 0;
+    other._usedCount = 0;
+    other._freeListHead = nullptr;
+
+    return *this;
+  }
+
+  void* allocate() {
+    assert(_usedCount < _capacity);
+    if (_freeListHead == nullptr) {
+      return nullptr;
+    }
+    void* result = _freeListHead;
+    _freeListHead = *reinterpret_cast<void**>(_freeListHead);  // move to the next
+    _usedCount++;
+    return result;
+  }
+
+  void deallocate(void* ptr) {
+    assert(owns(ptr));
+
+    // Put the deallocated block at the front of the free list
+    // and have it point to the previous head of the free list
+    *reinterpret_cast<void**>(ptr) = _freeListHead;
+    _freeListHead = ptr;
+    _usedCount--;
+  }
+
+  bool owns(void* ptr) const {
+    // check if the ptr is in range and that it is properly aligned
+    uintptr_t base = reinterpret_cast<uintptr_t>(_buffer);
+    uintptr_t p = reinterpret_cast<uintptr_t>(ptr);
+    uintptr_t offset = p - base;
+
+    return (ptr >= _buffer) && (ptr < _buffer + (_slotSize * _capacity)) &&
+           (offset % _slotSize == 0);
+  }
 
   size_t slotSize() const { return _slotSize; }
   size_t capacity() const { return _capacity; }
--- src/week08_stl_allocator_integration/PoolResource.hpp
+++ src/week08_stl_allocator_integration/PoolResource.hpp
@@ -7,14 +7,34 @@
 
 class PoolResource : public std::pmr::memory_resource {
  public:
-  PoolResource(PoolAllocator* allocator) : _allocator(allocator) {}
+  PoolResource(PoolAllocator* allocator) : _allocator(allocator) { assert(allocator != nullptr); }
 
-  T* do_allocate(size_t size, size_t alignment) override;
-  void do_deallocate(void* ptr, size_t size, size_t alignment) override;
+  ~PoolResource() = default;
+  PoolResource(const PoolResource&) = delete;
+  PoolResource& operator=(const PoolResource&) = delete;
 
-  bool do_is_equal(const memory_resource& other) const noexcept override {
-    return this == &other;
+  PoolResource(PoolResource&& other) : _allocator(other._allocator) { other._allocator = nullptr; }
+
+  PoolResource& operator=(PoolResource&& other) {
+    if (this == &other) return *this;
+    _allocator = other._allocator;
+    other._allocator = nullptr;
+    return *this;
   }
+
+  void* do_allocate(size_t size, size_t alignment) override {
+    auto ret = _allocator->allocate();
+    if (ret == nullptr) {
+      throw std::bad_alloc();
+    }
+    return ret;
+  }
+
+  void do_deallocate(void* ptr, size_t size, size_t alignment) override {
+    _allocator->deallocate(ptr);
+  }
+
+  bool do_is_equal(const memory_resource& other) const noexcept override { return this == &other; }
 
  private:
   PoolAllocator* _allocator;
