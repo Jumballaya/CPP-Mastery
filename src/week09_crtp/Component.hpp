924487aee66567e070d564d2a309cf3bda6d10be
Patrick Burris <nchupacabra@gmail.com>
2025-05-27 15:08:35 -0500

    Week09: Super basic ComponentRegistry

--- src/week09_crtp/Component.hpp
+++ src/week09_crtp/Component.hpp
@@ -1,26 +1,16 @@
 #pragma once
 
 #include <cstddef>
-#include <cstdint>
-#include <string>
 #include <string_view>
 
-using ComponentId = uint32_t;
-
-inline ComponentId nextComponentId = 0;
-
-template <typename T>
-ComponentId TypeId() {
-  static const ComponentId id = nextComponentId++;
-  return id;
-}
+#include "ComponentId.hpp"
 
 template <typename T>
 class Component {
  public:
   static ComponentId typeId() { return TypeId<T>(); }
 
-  static constexpr std::string_view name() { return typeNamel }
+  static constexpr std::string_view name() { return typeName; }
   static constexpr size_t sizeBytes() { return sizeof(T); }
 
  private:
--- src/week09_crtp/main.cpp
+++ src/week09_crtp/main.cpp
@@ -1,5 +1,43 @@
 #include <iostream>
 
+#include "ComponentRegistry.hpp"
+
+struct Transform : Component<Transform> {
+  float translation[3];
+  float rotation[4];
+  float scale[3];
+};
+struct Velocity : Component<Velocity> {
+  float x;
+  float y;
+  float z;
+};
+struct Health : Component<Health> {
+  float amount;
+};
+
+void demo_1_registry() {
+  ComponentRegistry registry;
+  registry.registerComponent<Transform>("Transform");
+  registry.registerComponent<Velocity>("Velocity");
+  registry.registerComponent<Health>("Health");
+
+  auto trans = registry.getInfo(Transform::typeId());
+  if (trans) {
+    std::cout << "Component: " << trans->name << ", size: " << trans->size << "\n";
+  }
+
+  auto vel = registry.getInfo(Velocity::typeId());
+  if (vel) {
+    std::cout << "Component: " << vel->name << ", size: " << vel->size << "\n";
+  }
+
+  auto health = registry.getInfo(Health::typeId());
+  if (health) {
+    std::cout << "Component: " << health->name << ", size: " << health->size << "\n";
+  }
+}
+
 int main() {
-  std::cout << "Week 09: CRTP" << std::endl;
+  demo_1_registry();
 }
