c4e682d6d78552a58efa43475871153a2e3c946c
Patrick Burris <nchupacabra@gmail.com>
2025-06-01 17:02:20 -0500

    WEEK10: Add Vec3f base with SIMD support

---
+++ src/week10_templates_simd/Vec3f.hpp
@@ -0,0 +1,42 @@
+#pragma once
+#include <xmmintrin.h>
+
+#include <cassert>
+#include <cstddef>
+#include <type_traits>
+
+struct alignas(16) Vec3f {
+  float x;
+  float y;
+  float z;
+  float _pad = 0.0f;  // pad to 16 bytes
+
+  Vec3f() : x(0.0f), y(0.0f), z(0.0f) {}
+  Vec3f(float _x, float _y, float _z) : x(_x), y(_y), z(_z) {}
+  ~Vec3f() = default;
+
+  static Vec3f fromSIMD(__m128 v) {
+    Vec3f result;
+    result.store(v);
+    return result;
+  }
+
+  // SIMD: load [x,y,z,pad] into __m128
+  inline __m128 load() const {
+    return _mm_load_ps(&x);
+  }
+
+  // SIMD: stores the __m128 back into this vector
+  inline void store(__m128 value) {
+    _mm_store_ps(&x, value);
+  }
+
+  // Expression Template entry
+  inline __m128 evaluate() const {
+    return load();
+  }
+};
+
+// Assert that Vec3f is aligned with SIMD needs
+static_assert(std::is_trivially_copyable_v<Vec3f>, "Vec3f must be trivially copyable");
+static_assert(sizeof(Vec3f) == 16, "Vec3f must be 16 bytes");
--- src/week10_templates_simd/main.cpp
+++ src/week10_templates_simd/main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
+#include "Vec3f.hpp"
+
+void demo_1_vec3f() {
+  Vec3f v1;
+  Vec3f v2(1.0f, 2.0f, 3.0f);
+  Vec3f v3{5.0f, 6.0f, 7.0f};
+
+  std::cout << "V1: < " << v1.x << ", " << v1.y << ", " << v1.z << " >" << std::endl;
+  std::cout << "V2: < " << v2.x << ", " << v2.y << ", " << v2.z << " >" << std::endl;
+  std::cout << "V3: < " << v3.x << ", " << v3.y << ", " << v3.z << " >" << std::endl;
+}
+
 int main() {
-  std::cout << "WEEK 10: Expression Templates & SIMD" << std::endl;
+  demo_1_vec3f();
 }
