7a1a3682d8405c0346363ec14e86d1fe90e3cdc9
Patrick Burris <nchupacabra@gmail.com>
2025-06-05 18:38:05 -0500

    WEEK11: Added System and SystemScheduler classes

--- src/week11_ecs_core/ecs/system/System.hpp
+++ src/week11_ecs_core/ecs/system/System.hpp
@@ -1,1 +1,10 @@
 #pragma once
+
+class World;
+
+class System {
+ public:
+  virtual ~System() = default;
+  virtual void update(World& world, float dt) = 0;
+  virtual const char* name() const { return "UNNAMED_SYSTEM"; }
+};
--- src/week11_ecs_core/ecs/system/SystemScheduler.hpp
+++ src/week11_ecs_core/ecs/system/SystemScheduler.hpp
@@ -1,3 +1,48 @@
 #pragma once
 
-class SystemScheduler {};
+#include <algorithm>
+#include <memory>
+#include <vector>
+
+#include "System.hpp"
+
+class World;
+
+class SystemScheduler {
+ public:
+  SystemScheduler() = default;
+  ~SystemScheduler() = default;
+  SystemScheduler(const SystemScheduler&) = delete;
+  SystemScheduler& operator=(const SystemScheduler&) = delete;
+  SystemScheduler(SystemScheduler&&) noexcept = default;
+  SystemScheduler& operator=(SystemScheduler&&) noexcept = default;
+
+  template <typename T, typename... Args>
+  void registerSystem(Args&&... args) {
+    static_assert(std::is_base_of_v<System, T>, "T must derive from System");
+    _systems.emplace_back(std::make_unique<T>(std::forward<Args>(args)...));
+  }
+
+  void update(World& world, float dt) {
+    for (auto& system : _systems) {
+      system->update(world, dt);
+    }
+  }
+
+  void clear() {
+    _systems.clear();
+  }
+
+  void removeSystem(System* system) {
+    auto it = std::remove_if(
+        _systems.begin(),
+        _systems.end(),
+        [system](const std::unique_ptr<System>& s) {
+          return s.get() == system;
+        });
+    _systems.erase(it, _systems.end());
+  }
+
+ private:
+  std::vector<std::unique_ptr<System>> _systems;
+};
