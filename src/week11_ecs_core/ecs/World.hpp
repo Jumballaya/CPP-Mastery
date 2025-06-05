1f1ad5da644c5dfcaacc5a2a33da89a1cc7ad929
Patrick Burris <nchupacabra@gmail.com>
2025-06-05 16:29:07 -0500

    WEEK11: Added entity and component creation to the World class

--- src/week11_ecs_core/ecs/World.hpp
+++ src/week11_ecs_core/ecs/World.hpp
@@ -1,13 +1,57 @@
 #pragma once
 
 #include "component/ComponentManager.hpp"
+#include "component/ComponentStorage.hpp"
+#include "entity/EntityId.hpp"
 #include "entity/EntityManager.hpp"
 #include "system/SystemScheduler.hpp"
 
 class World {
  public:
-  World();
-  ~World();
+  World() = default;
+  ~World() = default;
+  World(const World&) = delete;
+  World& operator=(const World&) = delete;
+  World(World&&) noexcept = default;
+  World& operator=(World&&) noexcept = default;
+
+  EntityId createEntity() { return _entityManager.create(); }
+  void destroyEntity(EntityId id) { _entityManager.destroy(id); }
+  bool isAlive(EntityId id) const { return _entityManager.isAlive(id); }
+
+  template <typename T, typename... Args>
+  void addComponent(EntityId id, Args&&... args) {
+    if (!isAlive(id)) {
+      return;
+    }
+    ComponentStorage<T>& storage = _componentManager.storage<T>();
+    storage.emplace(id, std::forward<Args>(args)...);
+  }
+
+  template <typename T>
+  [[nodiscard]]
+  T* getComponent(EntityId id) {
+    if (!isAlive(id)) {
+      return nullptr;
+    }
+    return _componentManager.get<T>(id);
+  }
+
+  template <typename T>
+  bool hasComponent(EntityId id) const {
+    if (!isAlive(id)) {
+      return false;
+    }
+    return _componentManager.has<T>(id);
+  }
+
+  template <typename T>
+  void removeComponent(EntityId id) {
+    if (!isAlive(id)) {
+      return;
+    }
+    _componentManager.remove<T>(id);
+  }
 
  private:
   EntityManager _entityManager;
