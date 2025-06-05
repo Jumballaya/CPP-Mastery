67f50a99dfcbe5c7ca6ad16380161b3e21afb91a
Patrick Burris <nchupacabra@gmail.com>
2025-06-05 16:14:13 -0500

    WEEK11: Added base World class

--- src/week11_ecs_core/ecs/World.hpp
+++ src/week11_ecs_core/ecs/World.hpp
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "component/ComponentManager.hpp"
+#include "entity/EntityManager.hpp"
+#include "system/SystemScheduler.hpp"
+
+class World {
+ public:
+  World();
+  ~World();
+
+ private:
+  EntityManager _entityManager;
+  ComponentManager _componentManager;
+  SystemScheduler _systemScheduler;
+};
