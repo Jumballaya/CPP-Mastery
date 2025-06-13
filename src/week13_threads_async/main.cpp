2069c5b6d06004f61b57b25131c26ac3f8084591
Patrick Burris <nchupacabra@gmail.com>
2025-06-12 22:50:58 -0500

    WEEK13: Simple Worker thread class

--- src/week13_threads_async/main.cpp
+++ src/week13_threads_async/main.cpp
@@ -1,5 +1,37 @@
 #include <iostream>
+#include <thread>
+#include <utility>
+
+class Work {
+ public:
+  template <typename Fn>
+  explicit Work(Fn&& fn) : t(std::forward<Fn>(fn)) {}
+
+  ~Work() {
+    if (t.joinable()) t.join();
+  }
+
+  Work(const Work&) = delete;
+  Work& operator=(const Work&) = delete;
+
+  Work(Work&& other) noexcept : t(std::move(other.t)) {}
+  Work& operator=(Work&& other) noexcept {
+    if (&other == this) return *this;
+    if (t.joinable()) t.join();
+    t = std::move(other.t);
+    return *this;
+  }
+
+  bool isRunning() const { return t.joinable(); }
+
+ private:
+  std::thread t;
+};
 
 int main() {
-  std::cout << "WEEK 13: Threads & Async" << std::endl;
+  Work w([]() {
+    std::cout << "Running on a new thread!" << std::endl;
+  });
+
+  std::cout << "Main thread!" << std::endl;
 }
