f95fe4a1e4d0bd549cbf6cd9dd412042fd2f615a
Patrick Burris <nchupacabra@gmail.com>
2025-06-24 10:27:58 -0500

    WEEK15: Testing the Job System more thoroughly

--- src/week15_fibers_task_graph/async/Job.hpp
+++ src/week15_fibers_task_graph/async/Job.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cassert>
+#include <cstddef>
 #include <new>
 #include <type_traits>
 #include <utility>
@@ -8,9 +9,47 @@
 template <size_t MaxSize = 64>
 struct Job {
   using FuncPtr = void (*)(void*);
+  using DestroyFuncPtr = void (*)(void*);
 
   alignas(std::max_align_t) char storage[MaxSize];
   FuncPtr func = nullptr;
+  DestroyFuncPtr destroyFunc = nullptr;
+
+  Job() = default;
+  ~Job() {
+    cleanup();
+  }
+  Job(const Job&) = delete;
+  Job& operator=(const Job&) = delete;
+
+  Job(Job&& other) noexcept {
+    func = other.func;
+    destroyFunc = other.destroyFunc;
+
+    for (size_t i = 0; i < MaxSize; ++i) {
+      storage[i] = other.storage[i];
+    }
+
+    other.func = nullptr;
+    other.destroyFunc = nullptr;
+  }
+
+  Job& operator=(Job&& other) noexcept {
+    if (this == &other) {
+      return *this;
+    }
+
+    cleanup();
+    func = other.func;
+    destroyFunc = other.destroyFunc;
+    for (size_t i = 0; i < MaxSize; ++i) {
+      storage[i] = other.storage[i];
+    }
+    other.func = nullptr;
+    other.destroyFunc = nullptr;
+
+    return *this;
+  }
 
   void operator()() const {
     assert(func && "Job function pointer is null");
@@ -20,7 +59,20 @@ struct Job {
   template <typename Fn>
   void set(Fn&& fn) {
     static_assert(sizeof(Fn) <= MaxSize, "Lambda is too large for job storage");
-    func = [](void* ptr) { (*reinterpret_cast<Fn*>(ptr))(); };
+    using LambdaType = std::decay_t<Fn>;
+
+    func = [](void* ptr) { (*reinterpret_cast<LambdaType*>(ptr))(); };
+    destroyFunc = [](void* ptr) { reinterpret_cast<LambdaType*>(ptr)->~LambdaType(); };
+
     new (storage) Fn(std::forward<Fn>(fn));
+  }
+
+ private:
+  void cleanup() {
+    if (destroyFunc) {
+      destroyFunc((void*)storage);
+      destroyFunc = nullptr;
+    }
+    func = nullptr;
   }
 };
--- src/week15_fibers_task_graph/main.cpp
+++ src/week15_fibers_task_graph/main.cpp
@@ -1,24 +1,90 @@
 #include <chrono>
+#include <cstdint>
 #include <iostream>
 #include <thread>
 
 #include "tasks/TaskGraph.hpp"
 
 void demo_1_task_graph_basic() {
-  TaskGraph graph;
+  int count = 0;
+  while (true) {
+    if (count > 10) break;
 
-  TaskId a = graph.addTask([] { std::cout << "Task A running" << std::endl; });
-  TaskId b = graph.addTask([] { std::cout << "Task B running" << std::endl; });
-  TaskId c = graph.addTask([] { std::cout << "Task C running" << std::endl; });
+    TaskGraph graph;
 
-  graph.addDependency(b, a);
-  graph.addDependency(c, b);
+    TaskId a = graph.addTask([] { std::cout << "Task A running" << std::endl; });
+    TaskId b = graph.addTask([] { std::cout << "Task B running" << std::endl; });
+    TaskId c = graph.addTask([] { std::cout << "Task C running" << std::endl; });
 
-  graph.execute();
+    graph.addDependency(b, a);
+    graph.addDependency(c, b);
 
-  std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    graph.execute();
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+    count++;
+  }
+}
+
+struct Message {
+  uint32_t id;
+  std::string msg;
+
+  Message() = default;
+
+  ~Message() {
+    std::cout << "Message: " << id << " destroyed" << std::endl;
+  }
+
+  Message(const Message& other) {
+    id = other.id + 1;
+    msg = other.msg;
+    std::cout << "Message copied [ctor]: " << other.id << std::endl;
+  }
+
+  Message& operator=(const Message& other) {
+    if (this == &other) return *this;
+
+    id = other.id + 1;
+    msg = other.msg;
+    std::cout << "Message copied [assign]: " << other.id << std::endl;
+
+    return *this;
+  }
+
+  friend std::ostream& operator<<(std::ostream& os, const Message& message) {
+    os << message.msg << ": " << message.id;
+    return os;
+  }
+};
+
+void demo_2_task_graph_scope_data() {
+  int count = 0;
+  while (true) {
+    if (count > 2) break;
+
+    TaskGraph graph;
+
+    Message message;
+    message.id = 0;
+    message.msg = "From Main Thread";
+
+    TaskId a = graph.addTask([message] { std::cout << "Task A running: " << message << std::endl; });
+    TaskId b = graph.addTask([message] { std::cout << "Task B running: " << message << std::endl; });
+    TaskId c = graph.addTask([message] { std::cout << "Task C running: " << message << std::endl; });
+
+    graph.addDependency(b, a);
+    graph.addDependency(c, b);
+
+    graph.execute();
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+    count++;
+  }
 }
 
 int main() {
-  demo_1_task_graph_basic();
+  demo_2_task_graph_scope_data();
 }
